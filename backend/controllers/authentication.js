8cc241ababb8ce710a97e546f5551bb8c9df08d9
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-11-23 19:01:39 -0800

    jwt assignment completed

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
 
+___  
 router.post('/', async (req, res) => {
+    
     let user = await User.findOne({
         where: { email: req.body.email }
     })
@@ -14,8 +17,29 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId } )
+        res.json({ user: user, token: result.value })                                       
     }
 })
+
+router.get('/profile', async (req, res) => {
+    const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+    if (authenticationMethod == 'Bearer') {
+        const result = await jwt.decode(process.env.JWT_SECRET, token)
+        const { id } = result.value
+    }
+    try {
+        let user = await User.findOne({
+            where: {
+                userId:id
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
 
 module.exports = router
